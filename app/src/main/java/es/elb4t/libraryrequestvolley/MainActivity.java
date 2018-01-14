dd3b912f8db22db387d16f34707ece86e4d92258
Elb4t <eloy4t@gmail.com>
2018-01-15 00:12:46 +0100

    Module app

---
+++ app/src/main/java/es/elb4t/libraryrequestvolley/MainActivity.java
@@ -0,0 +1,31 @@
+package es.elb4t.libraryrequestvolley;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        text = findViewById(R.id.textView);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+    }
+
+}
